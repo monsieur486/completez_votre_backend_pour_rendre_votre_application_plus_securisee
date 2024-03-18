1470a5ab47fe3ab40cad3877c5fd86d0e98cac50
monsieur486 <monsieur486@gmail.com>
2024-03-18 18:29:15 +0100

    add BidListRepository

--- src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -4,8 +4,12 @@ import com.nnk.springboot.domain.BidList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface BidListRepository extends JpaRepository<BidList, Integer> {
+
+    List<BidList> findBidListByAccount(String account);
 
 }
--- src/main/java/com/nnk/springboot/service/BidListService.java
+++ src/main/java/com/nnk/springboot/service/BidListService.java
@@ -4,25 +4,33 @@ import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BidListService {
 
     private final BidListRepository bidListRepository;
 
-
     public BidListService(BidListRepository bidListRepository) {
         this.bidListRepository = bidListRepository;
+    }
+
+
+    public List<BidList> findBidListByAccount(String account) {
+        return bidListRepository.findBidListByAccount(account);
     }
 
     public BidList findBidListById(Integer id) {
         return bidListRepository.findById(id).orElse(null);
     }
 
-    public BidList addBidList(BidList bidList) {
+    public BidList addBidList(BidList bidList, String username) {
+        bidList.setCreationName(username);
         return bidListRepository.save(bidList);
     }
 
-    public BidList updateBidList(BidList bidList) {
+    public BidList updateBidList(BidList bidList, String username) {
+        bidList.setRevisionName(username);
         return bidListRepository.save(bidList);
     }
 
