1429492ca303fcb5a17375f673c145e32aeff195
Monsieur 486 <monsieur486@gmail.com>
2024-03-18 14:22:47 +0100

    map BidList

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -16,5 +16,74 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @Table(name = "bidlist")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer BidListId;
+
+    @Column(nullable = false, length = 30)
+    private String account;
+
+    @Column(nullable = false, length = 30)
+    private String type;
+
+    @Min(value=1, message = "Bid Quantity must be greater than 1")
+    private Double bidQuantity;
+
+    private Double askQuantity;
+
+    private Double bid;
+
+    private Double ask;
+
+    @Column(length = 125)
+    private String benchmark;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp bidListDate;
+
+    @Column(length = 125)
+    private String commentary;
+
+    @Column(length = 125)
+    private String security;
+
+    @Column(length = 10)
+    private String status;
+
+    @Column(length = 125)
+    private String trader;
+
+    @Column(length = 125)
+    private String book;
+
+    @Column(length = 125)
+    private String creationName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp creationDate;
+
+    @Column(length = 125)
+    private String revisionName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp revisionDate;
+
+    @Column(length = 125)
+    private String dealName;
+
+    @Column(length = 125)
+    private String dealType;
+
+    @Column(length = 125)
+    private String sourceListId;
+
+    @Column(length = 125)
+    private String side;
+
+    public BidList(String accountTest, String account, double bidQuantity) {
+        this.account = accountTest;
+        this.type = account;
+        this.bidQuantity = bidQuantity;
+    }
 }
