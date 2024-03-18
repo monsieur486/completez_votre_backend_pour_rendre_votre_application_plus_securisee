f0762165441e8088ede9a09e04f436af64ed6635
Monsieur 486 <monsieur486@gmail.com>
2024-03-18 14:24:45 +0100

    map User

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -15,54 +15,22 @@ import lombok.Setter;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotBlank(message = "Username is mandatory")
+    @Column(length = 125)
     private String username;
+
     @NotBlank(message = "Password is mandatory")
+    @Column(length = 125)
     private String password;
+
     @NotBlank(message = "FullName is mandatory")
+    @Column(length = 125)
     private String fullname;
+
     @NotBlank(message = "Role is mandatory")
+    @Column(length = 125)
     private String role;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
