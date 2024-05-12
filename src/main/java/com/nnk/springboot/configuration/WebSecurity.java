420ccc1e733dff6b5961711ae57b7cd5b8b89f3c
Monsieur 486 <monsieur486@gmail.com>
2024-05-12 22:38:58 +0200

    ajust code

--- src/main/java/com/nnk/springboot/configuration/WebSecurity.java
+++ src/main/java/com/nnk/springboot/configuration/WebSecurity.java
@@ -4,6 +4,7 @@ import com.nnk.springboot.service.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity {
 
     @Bean
--- src/main/java/com/nnk/springboot/controllers/UserController.java
+++ src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -4,7 +4,7 @@ import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,20 +21,17 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping("/user/list")
-    @Secured("ROLE_ADMIN")
     public String home(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 
     @GetMapping("/user/add")
-    @Secured("ROLE_ADMIN")
     public String addUser(User bid) {
         return "user/add";
     }
 
     @PostMapping("/user/validate")
-    @Secured("ROLE_ADMIN")
     public String validate(@Valid User user, BindingResult result, Model model) {
         if (!result.hasErrors()) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -47,7 +44,6 @@ public class UserController {
     }
 
     @GetMapping("/user/update/{id}")
-    @Secured("ROLE_ADMIN")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         user.setPassword("");
@@ -56,7 +52,6 @@ public class UserController {
     }
 
     @PostMapping("/user/update/{id}")
-    @Secured("ROLE_ADMIN")
     public String updateUser(@PathVariable("id") Integer id, @Valid User user,
                              BindingResult result, Model model) {
         if (result.hasErrors()) {
@@ -72,7 +67,7 @@ public class UserController {
     }
 
     @GetMapping("/user/delete/{id}")
-    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         userRepository.delete(user);
