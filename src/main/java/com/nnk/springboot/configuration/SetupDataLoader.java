f4ab4b79ece04286452374d00ef5ca23e92eb0f7
Monsieur 486 <monsieur486@gmail.com>
2024-05-21 14:37:34 +0200

    add fixtures

--- src/main/java/com/nnk/springboot/configuration/SetupDataLoader.java
+++ src/main/java/com/nnk/springboot/configuration/SetupDataLoader.java
@@ -1,7 +1,6 @@
 package com.nnk.springboot.configuration;
 
-import com.nnk.springboot.domain.BidList;
-import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.configuration.fixture.InstallFixture;
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.dto.UserDto;
 import com.nnk.springboot.service.*;
@@ -18,12 +17,7 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
 
     private final UserService userService;
     private final PrivilegeService privilegeService;
-    private final BidListService bidListService;
-    private final RatingService ratingService;
-    private final RuleNameService ruleNameService;
-    private final TradeService tradeService;
-    private final CurvePointService curvePointService;
-
+    private final InstallFixture installFixture;
 
     @Value("${ADMIN_USERNAME}")
     public String adminUsername;
@@ -48,15 +42,12 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
 
     private boolean alreadySetup = false;
 
-    public SetupDataLoader(UserService userService, PrivilegeService privilegeService, BidListService bidListService, RatingService ratingService, RuleNameService ruleNameService, TradeService tradeService, CurvePointService curvePointService) {
+    public SetupDataLoader(UserService userService, PrivilegeService privilegeService, InstallFixture installFixture) {
         this.userService = userService;
         this.privilegeService = privilegeService;
-        this.bidListService = bidListService;
-        this.curvePointService = curvePointService;
-        this.ratingService = ratingService;
-        this.ruleNameService = ruleNameService;
-        this.tradeService = tradeService;
+        this.installFixture = installFixture;
     }
+
 
     @Override
     @Transactional
@@ -78,7 +69,11 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
             createUserIfNotFound("manager", "Manager", userPassword, "MANAGER");
             createPrivilegeIfNotFound("MANAGER", "USER_MANAGEMENT");
 
-            installFixtiures();
+            installFixture.installBidListFixture();
+            installFixture.installCurvePointFixture();
+            installFixture.installRatingFixture();
+            installFixture.installRuleNameFixture();
+            installFixture.installTradeFixture();
         }
 
         alreadySetup = true;
@@ -101,22 +96,4 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     private void createPrivilegeIfNotFound(String role, String name) {
         privilegeService.createIfNotFound(role, name);
     }
-
-    private void installFixtiures() {
-        log.warn("Installing fixtures");
-
-        log.warn("Creating fixtures for BidList");
-        BidList bidList = new BidList("Account Test", "Type Test", 10);
-        bidListService.saveBidList(bidList, "ADMIN");
-        BidList bidList2 = new BidList("Account Test 2", "Type Test 2", 20);
-        bidListService.saveBidList(bidList2, "ADMIN");
-
-        log.warn("Creating fixtures for CurevePoint");
-        curvePointService.saveCurvePoint(new CurvePoint(10, 10d, 30d));
-        curvePointService.saveCurvePoint(new CurvePoint(20, 20d, 40d));
-
-
-
-    }
-
 }
